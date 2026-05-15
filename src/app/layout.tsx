fde1dbce94428e1615d30ab8715c4aa782b6ed27
Hector Larrea <taller.mcmoto@gmail.com>
2026-05-15 03:06:07 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Space+Grotesk:wght@700;900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-slate-900 overflow-x-hidden md:py-12">
+      <body className="font-body antialiased bg-slate-900 overflow-x-hidden md:py-12" suppressHydrationWarning>
         {children}
       </body>
     </html>
